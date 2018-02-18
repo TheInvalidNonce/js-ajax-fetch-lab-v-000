1a983746dd3aea9902d7182fdb33b50c05543928
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-18 18:01:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 function getIssues() {
+  const repo = 'https://api.github.com/repos/TheInvalidNonce/javascript-fetch-lab/issues';
+
+  fetch(`${repo}`)
+    .then(resp => resp.json())
+    .then(json => json.map(issue => showIssues(issue)))
 }
 
-function showIssues(json) {
+function showIssues(issue) {
+  $('#issues').append(`<h3>New Issue</h3><a href="${issue.html_url}">${issue.html_url}</a>`)
 }
 
 function createIssue() {
+  const title = document.getElementById('title').value
+  const text = $('body').value
+  const issue = {title: title, body: text}
+
+  fetch(`${repo}`, {
+    method: 'post',
+    headers: {
+      Authorization: `${getToken()}`
+    },
+    body: JSON.stringify(issue)
+  }).then(resp => resp.json())
+    .then(json => console.log(json))
+    .then(getIssues())
 }
 
 function showResults(json) {
